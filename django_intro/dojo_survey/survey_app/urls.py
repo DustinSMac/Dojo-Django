48c46e547693bece6a5d2b9cd07dd0f595752565
Dustin Mac <sonnyduluxwatershield@gmail.com>
2021-05-03 08:19:39 -0500

    Adding redirect and session to survey app

--- django_intro/dojo_survey/survey_app/urls.py
+++ django_intro/dojo_survey/survey_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
+    path('submit',views.submit),
     path('result',views.result),
 ]
--- django_intro/dojo_survey/survey_app/views.py
+++ django_intro/dojo_survey/survey_app/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 
+def submit(request):
+    request.session['result']={
+        "name": request.POST["name"],
+        "location": request.POST["location"],
+        "language": request.POST["language"],
+        "comments": request.POST["comments"]
+    }
+    return redirect('/result')
+
 def result(request):
-    if request.method == 'POST':
-        context={
-            "name": request.POST["name"],
-            "location": request.POST["location"],
-            "language": request.POST["language"],
-            "comments": request.POST["comments"]
-        }
-        return render(request,"result.html",context)
-    return render(request,"result.html")
+    result= request.session['result']
+    return render(request,"result.html",result)
